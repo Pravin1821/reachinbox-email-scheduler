28dcdb6e4db2ca775983b821d22dd415d311284a
Pravin1821 <pravincriss21@gmail.com>
2026-08-30 06:16:03 +0530

    redis.ts file updated

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -3,6 +3,14 @@ import { env } from "./env";
 
 export const redisConnection = new Redis(env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  retryStrategy(times) {
+    return Math.min(times * 500, 5000); 
+  },
+  enableReadyCheck: true,
+  reconnectOnError(err) {
+    const targetErrors = ["READONLY", "ECONNRESET"];
+    return targetErrors.some((e) => err.message.includes(e));
+  },
 });
 
 redisConnection.on("connect", () => {
